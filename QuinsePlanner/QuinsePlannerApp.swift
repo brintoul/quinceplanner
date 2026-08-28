45e3c43ff5b773bfd0c1a2dae7421d48d11f1339
Bradley Rintoul <brintoul@controlledthinking.com>
2026-08-28 11:01:34 -0700

    Initial Commit

---
+++ QuinsePlanner/QuinsePlannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuinsePlannerApp.swift
+//  QuinsePlanner
+//
+//  Created by Bradley Rintoul on 8/28/26.
+//
+
+import SwiftUI
+
+@main
+struct QuinsePlannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
